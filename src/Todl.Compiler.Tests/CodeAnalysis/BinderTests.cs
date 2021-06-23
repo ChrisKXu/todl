685cb98dc28354165a1a5a18e35b89fc856f9640
Christopher Xu <chriskxu.tictac@gmail.com>
2021-06-22 18:29:39 -0700

    Add tests for Evaluator

---
+++ src/Todl.Compiler.Tests/CodeAnalysis/BinderTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todl.Compiler.Tests.CodeAnalysis
+{
+    public sealed class BinderTests
+    {
+    }
+}
