b087db7b1e15ca900f0bcb10314d397d5f1d49f3
Christopher Xu <chriskxu.tictac@gmail.com>
2021-07-01 22:15:15 -0700

    Move Binder logic to respective files

--- src/Todl.Compiler/CodeAnalysis/Binding/Binder.cs
+++ src/Todl.Compiler/CodeAnalysis/Binding/Binder.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Todl.Compiler.CodeAnalysis.Symbols;
 using Todl.Compiler.CodeAnalysis.Syntax;
+using Todl.Compiler.Diagnostics;
 
 namespace Todl.Compiler.CodeAnalysis.Binding
 {
@@ -12,117 +13,10 @@ namespace Todl.Compiler.CodeAnalysis.Binding
     /// Binder reorganizes SyntaxTree elements into Bound counterparts
     /// and prepares necessary information for Emitter to use in the emit process
     /// </summary>
-    public sealed class Binder
+    public sealed partial class Binder
     {
-        internal BoundExpression BindExpression(Expression expression)
-        {
-            return expression switch
-            {
-                LiteralExpression literalExpression => this.BindLiteralExpression(literalExpression),
-                BinaryExpression binaryExpression => this.BindBinaryExpression(binaryExpression),
-                UnaryExpression unaryExpression => this.BindUnaryExpression(unaryExpression),
-                ParethesizedExpression parethesizedExpression => this.BindExpression(parethesizedExpression.InnerExpression),
-                _ => throw new NotImplementedException()
-            };
-        }
+        private readonly List<Diagnostic> diagnostics = new();
 
-        private BoundConstant BindLiteralExpression(LiteralExpression literalExpression)
-        {
-            return literalExpression.LiteralToken.Kind switch
-            {
-                SyntaxKind.NumberToken => this.BindNumericConstant(literalExpression.LiteralToken),
-                SyntaxKind.StringToken => this.BindStringConstant(literalExpression.LiteralToken),
-                SyntaxKind.TrueKeywordToken or SyntaxKind.FalseKeywordToken => new BoundConstant(TypeSymbol.ClrBoolean, bool.Parse(literalExpression.LiteralToken.Text.ToReadOnlyTextSpan())),
-                _ => throw new NotSupportedException($"Literal value {literalExpression.LiteralToken.Text} is not supported"),
-            };
-        }
-
-        private BoundConstant BindNumericConstant(SyntaxToken syntaxToken)
-        {
-            var text = syntaxToken.Text.ToReadOnlyTextSpan();
-
-            if (int.TryParse(text, out var parsedInt))
-            {
-                return new BoundConstant(TypeSymbol.ClrInt32, parsedInt);
-            }
-
-            if (double.TryParse(text, out var parsedDouble))
-            {
-                return new BoundConstant(TypeSymbol.ClrDouble, parsedDouble);
-            }
-
-            throw new NotSupportedException($"Literal value {syntaxToken.Text} is not supported");
-        }
-
-        private BoundConstant BindStringConstant(SyntaxToken syntaxToken)
-        {
-            var text = syntaxToken.Text.ToReadOnlyTextSpan();
-            var escape = text[0] != '@';
-            var builder = new StringBuilder();
-
-            for (var i = escape ? 1 : 2; i < text.Length - 1; ++i)
-            {
-                var c = text[i];
-                if (escape && c == '\\')
-                {
-                    switch (text[++i])
-                    {
-                        case 't':
-                            builder.Append('\t');
-                            break;
-                        case 'r':
-                            builder.Append('\r');
-                            break;
-                        case 'n':
-                            builder.Append('\n');
-                            break;
-                        case '\\':
-                            builder.Append('\\');
-                            break;
-                        case '"':
-                            builder.Append('"');
-                            break;
-                        default:
-                            throw new NotSupportedException($"Literal value {syntaxToken.Text} is not supported");
-                    }
-                }
-                else
-                {
-                    builder.Append(c);
-                }
-            }
-
-            return new BoundConstant(TypeSymbol.ClrString, builder.ToString());
-        }
-
-        private BoundUnaryExpression BindUnaryExpression(UnaryExpression unaryExpression)
-        {
-            var boundOperand = this.BindExpression(unaryExpression.Operand);
-            var boundUnaryOperator = BoundUnaryExpression.MatchUnaryOperator(
-                operandResultType: boundOperand.ResultType,
-                syntaxKind: unaryExpression.Operator.Kind,
-                trailing: unaryExpression.Trailing);
-
-            if (boundUnaryOperator == null)
-            {
-                throw new NotSupportedException($"Operator {unaryExpression.Operator.Text} is not supported on type {boundOperand.ResultType.Name}");
-            }
-
-            return new BoundUnaryExpression(boundUnaryOperator, boundOperand);
-        }
-
-        private BoundBinaryExpression BindBinaryExpression(BinaryExpression binaryExpression)
-        {
-            var boundLeft = this.BindExpression(binaryExpression.Left);
-            var boundRight = this.BindExpression(binaryExpression.Right);
-            var boundBinaryOperator = BoundBinaryExpression.MatchBinaryOperator(boundLeft.ResultType, boundRight.ResultType, binaryExpression.Operator.Kind);
-
-            if (boundBinaryOperator == null)
-            {
-                throw new NotSupportedException($"Operator {binaryExpression.Operator.Text} is not supported on types {boundLeft.ResultType.Name} and {boundRight.ResultType.Name}");
-            }
-
-            return new BoundBinaryExpression(boundBinaryOperator, boundLeft, boundRight);
-        }
+        public IReadOnlyList<Diagnostic> Diagnostics => diagnostics;
     }
 }
--- src/Todl.Compiler/CodeAnalysis/Binding/BoundExpression.cs
+++ src/Todl.Compiler/CodeAnalysis/Binding/BoundExpression.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Todl.Compiler.CodeAnalysis.Symbols;
+using Todl.Compiler.CodeAnalysis.Syntax;
 
 namespace Todl.Compiler.CodeAnalysis.Binding
 {
     public abstract class BoundExpression
     {
         public abstract TypeSymbol ResultType { get; }
+    }
+
+    public sealed partial class Binder
+    {
+        internal BoundExpression BindExpression(Expression expression)
+        {
+            return expression switch
+            {
+                LiteralExpression literalExpression => this.BindLiteralExpression(literalExpression),
+                BinaryExpression binaryExpression => this.BindBinaryExpression(binaryExpression),
+                UnaryExpression unaryExpression => this.BindUnaryExpression(unaryExpression),
+                ParethesizedExpression parethesizedExpression => this.BindExpression(parethesizedExpression.InnerExpression),
+                _ => throw new NotImplementedException()
+            };
+        }
     }
 }
