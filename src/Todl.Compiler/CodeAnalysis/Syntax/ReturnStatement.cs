5c688a8229cfc345e0b42162b0d4948012799720
Christopher Xu <chriskxu.tictac@gmail.com>
2021-11-22 19:43:14 -0800

    Added parser logic for 'ReturnStatement'

---
+++ src/Todl.Compiler/CodeAnalysis/Syntax/ReturnStatement.cs
@@ -0,0 +1,36 @@
+﻿using Todl.Compiler.CodeAnalysis.Text;
+
+namespace Todl.Compiler.CodeAnalysis.Syntax;
+
+public sealed class ReturnStatement : Statement
+{
+    public SyntaxToken ReturnKeywordToken { get; internal init; }
+    public Expression ReturnValueExpression { get; internal init; }
+    public SyntaxToken SemicolonToken { get; internal init; }
+
+    public override TextSpan Text => TextSpan.FromTextSpans(ReturnKeywordToken.Text, SemicolonToken.Text);
+}
+
+public sealed partial class Parser
+{
+    private ReturnStatement ParseReturnStatement()
+    {
+        var returnKeywordToken = ExpectToken(SyntaxKind.ReturnKeywordToken);
+        Expression returnValueExpression = null;
+
+        if (Current.Kind != SyntaxKind.SemicolonToken)
+        {
+            returnValueExpression = ParseExpression();
+        }
+
+        var semicolonToken = ExpectToken(SyntaxKind.SemicolonToken);
+
+        return new()
+        {
+            SyntaxTree = syntaxTree,
+            ReturnKeywordToken = returnKeywordToken,
+            ReturnValueExpression = returnValueExpression,
+            SemicolonToken = semicolonToken
+        };
+    }
+}
