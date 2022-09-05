aec5649f658ab03ec953590b18952e6017fe3f9c
Chris Xu <chriskxu.tictac@gmail.com>
2022-09-05 16:06:03 -0700

    Initial project setup

---
+++ src/Todl.Playground/Functions/InfoFunction.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Todl.Playground.Functions;
+
+public class InfoFunction
+{
+    [FunctionName("info")]
+    public IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest request,
+        ILogger logger)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        return new OkObjectResult(new { Status = "Running" });
+    }
+}
