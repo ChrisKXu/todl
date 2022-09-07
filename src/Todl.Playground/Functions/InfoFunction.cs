93ece47345e48aef043e81ba36d418b92bc17f0d
Chris Xu <chriskxu.tictac@gmail.com>
2022-09-06 21:31:47 -0700

    Implement InfoFunction

--- src/Todl.Playground/Functions/InfoFunction.cs
+++ src/Todl.Playground/Functions/InfoFunction.cs
@@ -3,18 +3,37 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Todl.Playground.Functions;
 
 public class InfoFunction
 {
+    private static readonly Assembly assembly = Assembly.GetExecutingAssembly();
+
     [FunctionName("info")]
     public IActionResult Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest request,
         ILogger logger)
     {
-        logger.LogInformation("C# HTTP trigger function processed a request.");
-
-        return new OkObjectResult(new { Status = "Running" });
+        return new OkObjectResult(new
+        {
+            RuntimeInfo = new
+            {
+                OSEnvironment = Environment.OSVersion,
+                Runtime = RuntimeInformation.FrameworkDescription,
+                Architecture = RuntimeInformation.ProcessArchitecture.ToString()
+            },
+            BuildInfo = new
+            {
+                Debug = assembly.GetCustomAttribute<DebuggableAttribute>() is not null,
+                Version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion
+            }
+        });
     }
 }
