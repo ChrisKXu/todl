302ae02d187bb188964d3cce555310402d78a5b0
Chris Xu <chriskxu.tictac@gmail.com>
2023-09-02 20:59:06 -0700

    Implement correct behavior for unary and assignment expressions (#91)
    * Implement correct behavior for unary and assignment expressions
    
    * Allow MemberAccessExpression to carry postfix operators

--- src/Todl.Compiler.Tests/CodeGeneration/EmitAssignmentExpressionTests.cs
+++ src/Todl.Compiler.Tests/CodeGeneration/EmitAssignmentExpressionTests.cs
@@ -51,6 +51,302 @@ public sealed class EmitAssignmentExpressionTests
     }
 
     [Fact]
+    public void TestEmitAssignmentExpressionWithLocalVariablesWithSideEffectPrefixIncrement()
+    {
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1; let b = ++a; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1U; let b = ++a; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1L; let b = ++a; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1UL; let b = ++a; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1.0F; let b = ++a; }",
+            TestInstruction.Create(OpCodes.Ldc_R4, 1F),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Ldc_R4, 1F),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1.0; let b = ++a; }",
+            TestInstruction.Create(OpCodes.Ldc_R8, 1.0),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Ldc_R8, 1.0),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+    }
+
+    [Fact]
+    public void TestEmitAssignmentExpressionWithLocalVariablesWithSideEffectPrefixDecrement()
+    {
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1; let b = --a; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Sub),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1U; let b = --a; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Sub),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1L; let b = --a; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Sub),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1UL; let b = --a; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Sub),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1.0F; let b = --a; }",
+            TestInstruction.Create(OpCodes.Ldc_R4, 1F),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Ldc_R4, 1F),
+            TestInstruction.Create(OpCodes.Sub),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1.0; let b = --a; }",
+            TestInstruction.Create(OpCodes.Ldc_R8, 1.0),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Ldc_R8, 1.0),
+            TestInstruction.Create(OpCodes.Sub),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+    }
+
+    [Fact]
+    public void TestEmitAssignmentExpressionWithLocalVariablesWithSideEffectPostfixIncrement()
+    {
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1; let b = a++; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1U; let b = a++; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1L; let b = a++; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1UL; let b = a++; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1.0F; let b = a++; }",
+            TestInstruction.Create(OpCodes.Ldc_R4, 1F),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Ldc_R4, 1F),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1.0; let b = a++; }",
+            TestInstruction.Create(OpCodes.Ldc_R8, 1.0),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Ldc_R8, 1.0),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+    }
+
+    [Fact]
+    public void TestEmitAssignmentExpressionWithLocalVariablesWithSideEffectPostfixDecrement()
+    {
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1; let b = a--; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Sub),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1U; let b = a--; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Sub),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1L; let b = a--; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Sub),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1UL; let b = a--; }",
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Conv_I8),
+            TestInstruction.Create(OpCodes.Sub),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1.0F; let b = a--; }",
+            TestInstruction.Create(OpCodes.Ldc_R4, 1F),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Ldc_R4, 1F),
+            TestInstruction.Create(OpCodes.Sub),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+
+        TestUtils.EmitStatementAndVerify(
+            "{ let a = 1.0; let b = a--; }",
+            TestInstruction.Create(OpCodes.Ldc_R8, 1.0),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Ldloc_0),
+            TestInstruction.Create(OpCodes.Dup),
+            TestInstruction.Create(OpCodes.Ldc_R8, 1.0),
+            TestInstruction.Create(OpCodes.Sub),
+            TestInstruction.Create(OpCodes.Stloc_0),
+            TestInstruction.Create(OpCodes.Stloc_1));
+    }
+
+    [Fact]
     public void TestEmitAssignmentExpressionWithStaticFields()
     {
         TestUtils.EmitStatementAndVerify(
@@ -108,5 +404,45 @@ public sealed class EmitAssignmentExpressionTests
             TestInstruction.Create(OpCodes.Ldsfld, "Todl.Compiler.Tests.TestClass Instance"),
             TestInstruction.Create(OpCodes.Ldstr, "abc"),
             TestInstruction.Create(OpCodes.Callvirt, "System.Void Todl.Compiler.Tests.TestClass::set_PublicStringProperty(System.String)"));
+    }
+
+    [Fact]
+    public void TestEmitAssignmentExpressionWithSelfAssignmentInstanceFields()
+    {
+        TestUtils.EmitStatementAndVerify(
+            "Todl.Compiler.Tests.TestClass.Instance.PublicIntField = Todl.Compiler.Tests.TestClass.Instance.PublicIntField;",
+            TestInstruction.Create(OpCodes.Ldsfld, "Todl.Compiler.Tests.TestClass Instance"),
+            TestInstruction.Create(OpCodes.Ldsfld, "Todl.Compiler.Tests.TestClass Instance"),
+            TestInstruction.Create(OpCodes.Ldfld, "System.Int32 PublicIntField"),
+            TestInstruction.Create(OpCodes.Stfld, "System.Int32 PublicIntField"));
+
+        TestUtils.EmitStatementAndVerify(
+            "Todl.Compiler.Tests.TestClass.Instance.PublicIntField = Todl.Compiler.Tests.TestClass.Instance.PublicIntField + 1;",
+            TestInstruction.Create(OpCodes.Ldsfld, "Todl.Compiler.Tests.TestClass Instance"),
+            TestInstruction.Create(OpCodes.Ldsfld, "Todl.Compiler.Tests.TestClass Instance"),
+            TestInstruction.Create(OpCodes.Ldfld, "System.Int32 PublicIntField"),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Stfld, "System.Int32 PublicIntField"));
+    }
+
+    [Fact]
+    public void TestEmitAssignmentExpressionWithSelfAssignmentInstanceProperties()
+    {
+        TestUtils.EmitStatementAndVerify(
+            "Todl.Compiler.Tests.TestClass.Instance.PublicIntProperty = Todl.Compiler.Tests.TestClass.Instance.PublicIntProperty;",
+            TestInstruction.Create(OpCodes.Ldsfld, "Todl.Compiler.Tests.TestClass Instance"),
+            TestInstruction.Create(OpCodes.Ldsfld, "Todl.Compiler.Tests.TestClass Instance"),
+            TestInstruction.Create(OpCodes.Callvirt, "System.Int32 Todl.Compiler.Tests.TestClass::get_PublicIntProperty()"),
+            TestInstruction.Create(OpCodes.Callvirt, "System.Void Todl.Compiler.Tests.TestClass::set_PublicIntProperty(System.Int32)"));
+
+        TestUtils.EmitStatementAndVerify(
+            "Todl.Compiler.Tests.TestClass.Instance.PublicIntProperty = Todl.Compiler.Tests.TestClass.Instance.PublicIntProperty + 1;",
+            TestInstruction.Create(OpCodes.Ldsfld, "Todl.Compiler.Tests.TestClass Instance"),
+            TestInstruction.Create(OpCodes.Ldsfld, "Todl.Compiler.Tests.TestClass Instance"),
+            TestInstruction.Create(OpCodes.Callvirt, "System.Int32 Todl.Compiler.Tests.TestClass::get_PublicIntProperty()"),
+            TestInstruction.Create(OpCodes.Ldc_I4_1),
+            TestInstruction.Create(OpCodes.Add),
+            TestInstruction.Create(OpCodes.Callvirt, "System.Void Todl.Compiler.Tests.TestClass::set_PublicIntProperty(System.Int32)"));
     }
 }
--- src/Todl.Compiler.Tests/TestClass.cs
+++ src/Todl.Compiler.Tests/TestClass.cs
@@ -4,15 +4,20 @@ public class TestClass
 {
     public string PublicStringField;
     public int PublicIntField;
+    public bool PublicBoolField;
 
     public static string PublicStaticStringField;
     public static int PublicStaticIntField;
+    public static bool PublicStaticBoolField;
 
     public string PublicStringProperty { get; set; }
     public int PublicIntProperty { get; set; }
+    public bool PublicBoolProperty { get; set; }
 
     public static string PublicStaticStringProperty { get; set; }
     public static int PublicStaticIntProperty { get; set; }
+    public static bool PublicStaticBoolProperty { get; set; }
+
 
     public static readonly TestClass Instance = new();
 }
