72ada56f9a9ee325d07e72f83373b08621742214
Chris Xu <chriskxu.tictac@gmail.com>
2023-10-21 17:01:23 -0700

    Adding parser support for WhileUntilStatement, BreakStatement and ContinueStatement

---
+++ src/Todl.Compiler.Tests/CodeAnalysis/ParserTests/WhileUntilStatementTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using Todl.Compiler.CodeAnalysis.Syntax;
+using Xunit;
+
+namespace Todl.Compiler.Tests.CodeAnalysis;
+
+public sealed class WhileUntilStatementTests
+{
+    [Fact]
+    public void EmptyBreakStatementCanBeParsed()
+    {
+        var breakStatement = TestUtils.ParseStatement<BreakStatement>("break;");
+        breakStatement.Should().NotBeNull();
+        breakStatement.GetDiagnostics().Should().BeEmpty();
+
+        breakStatement.BreakKeywordToken.Kind.Should().Be(SyntaxKind.BreakKeywordToken);
+        breakStatement.SemicolonToken.Kind.Should().Be(SyntaxKind.SemicolonToken);
+        breakStatement.Text.Length.Should().Be(6);
+    }
+
+    [Fact]
+    public void EmptyContinueStatementCanBeParsed()
+    {
+        var continueStatement = TestUtils.ParseStatement<ContinueStatement>("continue;");
+        continueStatement.Should().NotBeNull();
+        continueStatement.GetDiagnostics().Should().BeEmpty();
+
+        continueStatement.ContinueKeywordToken.Kind.Should().Be(SyntaxKind.ContinueKeywordToken);
+        continueStatement.SemicolonToken.Kind.Should().Be(SyntaxKind.SemicolonToken);
+        continueStatement.Text.Length.Should().Be(9);
+    }
+
+    [Theory]
+    [InlineData("while n == 0 { }", SyntaxKind.WhileKeywordToken)]
+    [InlineData("until n == 0 { }", SyntaxKind.UntilKeywordToken)]
+    public void WhileUntilStatementsCanHaveEmptyBody(string inputText, SyntaxKind expectedSyntaxKind)
+    {
+        var whileUntilStatement = TestUtils.ParseStatement<WhileUntilStatement>(inputText);
+        whileUntilStatement.Should().NotBeNull();
+        whileUntilStatement.GetDiagnostics().Should().BeEmpty();
+
+        whileUntilStatement.WhileOrUntilToken.Kind.Should().Be(expectedSyntaxKind);
+        whileUntilStatement.BlockStatement.InnerStatements.Should().BeEmpty();
+
+        var condition = whileUntilStatement.ConditionExpression.As<BinaryExpression>();
+        condition.Should().NotBeNull();
+        condition.Left.As<NameExpression>().Text.ToString().Should().Be("n");
+        condition.Operator.Kind.Should().Be(SyntaxKind.EqualsEqualsToken);
+        condition.Right.As<LiteralExpression>().Text.ToString().Should().Be("0");
+    }
+
+    [Theory]
+    [InlineData("while n == 0 { return n; }", 1)]
+    [InlineData("until n == 0 { n +=1; return n; }", 2)]
+    public void WhileUntilStatementsCanHaveOneOrMoreInnerStatements(string inputText, int expectedStatementsCount)
+    {
+        var whileUntilStatement = TestUtils.ParseStatement<WhileUntilStatement>(inputText);
+        whileUntilStatement.Should().NotBeNull();
+        whileUntilStatement.GetDiagnostics().Should().BeEmpty();
+        whileUntilStatement.BlockStatement.InnerStatements.Count.Should().Be(expectedStatementsCount);
+    }
+}
---
+++ src/Todl.Compiler/CodeAnalysis/Syntax/BreakStatement.cs
@@ -0,0 +1,25 @@
+﻿using Todl.Compiler.CodeAnalysis.Text;
+
+namespace Todl.Compiler.CodeAnalysis.Syntax;
+
+public sealed class BreakStatement : Statement
+{
+    public SyntaxToken BreakKeywordToken { get; internal init; }
+    public SyntaxToken SemicolonToken { get; internal init; }
+
+    public override TextSpan Text
+        => TextSpan.FromTextSpans(BreakKeywordToken.Text, SemicolonToken.Text);
+}
+
+public sealed partial class Parser
+{
+    private BreakStatement ParseBreakStatement()
+    {
+        return new()
+        {
+            SyntaxTree = syntaxTree,
+            BreakKeywordToken = ExpectToken(SyntaxKind.BreakKeywordToken),
+            SemicolonToken = ExpectToken(SyntaxKind.SemicolonToken)
+        };
+    }
+}
