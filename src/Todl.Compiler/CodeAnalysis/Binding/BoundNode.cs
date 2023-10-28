8235c75c3a9d25e3baf2a1eda16c142d47334040
Chris Xu <chriskxu.tictac@gmail.com>
2023-10-28 15:02:11 -0700

    [Engineering] Change BoundNodeFactorySourceGenerator to be IIncrementalSourceGenerator

--- src/Todl.Compiler/CodeAnalysis/Binding/BoundNode.cs
+++ src/Todl.Compiler/CodeAnalysis/Binding/BoundNode.cs
@@ -14,6 +14,7 @@ public abstract class BoundNode : IDiagnosable
         if (DiagnosticBuilder == null)
         {
             return DiagnosticBag.Empty;
+
         }
 
         return DiagnosticBuilder.Build();
