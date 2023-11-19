f23510da8713c3121814150f0248ee9373fb7bc4
Chris Xu <chriskxu.tictac@gmail.com>
2023-11-18 20:31:49 -0800

    [Bugfix] Fix null-ref in fibonacci sample (#99)

--- src/Todl.Compiler/CodeGeneration/Emitter.Expressions.cs
+++ src/Todl.Compiler/CodeGeneration/Emitter.Expressions.cs
@@ -153,7 +153,15 @@ internal partial class Emitter
 
             if (!boundClrFunctionCallExpression.IsStatic)
             {
-                EmitExpression(boundClrFunctionCallExpression.BoundBaseExpression);
+                if (boundClrFunctionCallExpression.BoundBaseExpression is BoundVariableExpression boundVariableExpression
+                    && boundVariableExpression.Variable is LocalVariableSymbol localVariableSymbol)
+                {
+                    EmitLocalAddress(localVariableSymbol);
+                }
+                else
+                {
+                    EmitExpression(boundClrFunctionCallExpression.BoundBaseExpression);
+                }
             }
 
             var methodReference = ResolveMethodReference(boundClrFunctionCallExpression);
@@ -248,7 +256,7 @@ internal partial class Emitter
             var slot = variables[localVariableSymbol].Index;
             if (slot < 0xFF)
             {
-                ILProcessor.Emit(OpCodes.Ldloca_S, (sbyte)slot);
+                ILProcessor.Emit(OpCodes.Ldloca_S, (byte)slot);
             }
             else
             {
