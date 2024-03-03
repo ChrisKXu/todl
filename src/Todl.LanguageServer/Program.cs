f2dfac91555ca82750cf4579706014e8763c59b3
Chris Xu <chriskxu.tictac@gmail.com>
2024-03-03 11:33:13 -0800

    Add Todl.LanguageServer project

---
+++ src/Todl.LanguageServer/Program.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+
+namespace Todl.LanguageServer;
+
+internal class Program
+{
+    public static Task Main(string[] args)
+    {
+        return Task.Delay(100);
+    }
+}
