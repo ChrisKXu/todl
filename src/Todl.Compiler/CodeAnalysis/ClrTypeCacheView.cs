df5fd79eee60e99f7af534810c7459b5cfc434a0
Chris Xu <chriskxu.tictac@gmail.com>
2024-08-05 21:33:47 -0700

    change ClrTypeCache, ClrTypeCacheView and symbol classes to use ImmutableArrays

--- src/Todl.Compiler/CodeAnalysis/ClrTypeCacheView.cs
+++ src/Todl.Compiler/CodeAnalysis/ClrTypeCacheView.cs
@@ -1,86 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using Todl.Compiler.CodeAnalysis.Symbols;
 using Todl.Compiler.CodeAnalysis.Syntax;
 
-namespace Todl.Compiler.CodeAnalysis
+namespace Todl.Compiler.CodeAnalysis;
+
+public sealed class ClrTypeCacheView
 {
-    public sealed class ClrTypeCacheView
+    private readonly ClrTypeCache clrTypeCache;
+    private readonly ImmutableDictionary<string, ClrTypeSymbol> typeAliases;
+
+    internal ClrTypeSymbol ResolveBaseType(string name)
     {
-        private readonly ClrTypeCache clrTypeCache;
-        private readonly IDictionary<string, ClrTypeSymbol> typeAliases;
-
-        internal ClrTypeSymbol ResolveBaseType(string name)
+        if (string.IsNullOrEmpty(name))
         {
-            if (string.IsNullOrEmpty(name))
-            {
-                throw new ArgumentNullException(nameof(name));
-            }
-
-            if (typeAliases.ContainsKey(name))
-            {
-                return typeAliases[name];
-            }
-
-            return clrTypeCache.Resolve(name);
+            throw new ArgumentNullException(nameof(name));
         }
 
-        public ClrTypeSymbol ResolveType(NameExpression nameExpression)
-            => ResolveBaseType(nameExpression.Text.ToString());
-
-        public ClrTypeSymbol ResolveType(TypeExpression typeExpression)
+        if (typeAliases.ContainsKey(name))
         {
-            var baseType = ResolveType(typeExpression.BaseTypeExpression);
-            if (!typeExpression.IsArrayType)
-            {
-                return baseType;
-            }
-
-            if (baseType is null)
-            {
-                return null;
-            }
-
-            var resolvedTypeString = typeExpression.Text.ToString().Replace(typeExpression.BaseTypeExpression.Text.ToString(), baseType.Name);
-
-            return new(baseType.ClrType.Assembly.GetType(resolvedTypeString));
+            return typeAliases[name];
         }
 
-        private IDictionary<string, ClrTypeSymbol> ImportTypeAliases(IEnumerable<ImportDirective> importDirectives)
+        return clrTypeCache.Resolve(name);
+    }
+
+    public ClrTypeSymbol ResolveType(NameExpression nameExpression)
+        => ResolveBaseType(nameExpression.Text.ToString());
+
+    public ClrTypeSymbol ResolveType(TypeExpression typeExpression)
+    {
+        var baseType = ResolveType(typeExpression.BaseTypeExpression);
+        if (!typeExpression.IsArrayType)
         {
-            if (importDirectives == null)
+            return baseType;
+        }
+
+        if (baseType is null)
+        {
+            return null;
+        }
+
+        var resolvedTypeString = typeExpression.Text.ToString().Replace(typeExpression.BaseTypeExpression.Text.ToString(), baseType.Name);
+
+        return new(baseType.ClrType.Assembly.GetType(resolvedTypeString));
+    }
+
+    private ImmutableDictionary<string, ClrTypeSymbol> ImportTypeAliases(IEnumerable<ImportDirective> importDirectives)
+    {
+        if (importDirectives == null)
+        {
+            throw new ArgumentNullException(nameof(importDirectives));
+        }
+
+        var importedTypes = importDirectives.SelectMany(importDirective =>
+        {
+            var importedNamespace = importDirective.Namespace.ToString();
+            var types = clrTypeCache
+                .Types
+                .Where(t => importedNamespace.Equals(t.Namespace));
+
+            if (!importDirective.ImportAll)
             {
-                throw new ArgumentNullException(nameof(importDirectives));
+                var importedNames = importDirective
+                    .ImportedNames
+                    .Select(n => $"{importedNamespace}.{n}")
+                    .ToHashSet();
+
+                types = types.Where(t => importedNames.Contains(t.Name));
             }
 
-            var importedTypes = importDirectives.SelectMany(importDirective =>
-            {
-                var importedNamespace = importDirective.Namespace.ToString();
-                var types = clrTypeCache
-                    .Types
-                    .Where(t => importedNamespace.Equals(t.Namespace));
+            return types;
+        }).Distinct();
 
-                if (!importDirective.ImportAll)
-                {
-                    var importedNames = importDirective
-                        .ImportedNames
-                        .Select(n => $"{importedNamespace}.{n}")
-                        .ToHashSet();
+        return importedTypes.ToImmutableDictionary(t => t.ClrType.Name);
+    }
 
-                    types = types.Where(t => importedNames.Contains(t.Name));
-                }
-
-                return types;
-            }).Distinct();
-
-            return importedTypes.ToDictionary(t => t.ClrType.Name);
-        }
-
-        internal ClrTypeCacheView(ClrTypeCache cache, IEnumerable<ImportDirective> importDirectives)
-        {
-            clrTypeCache = cache;
-            typeAliases = ImportTypeAliases(importDirectives);
-        }
+    internal ClrTypeCacheView(ClrTypeCache cache, IEnumerable<ImportDirective> importDirectives)
+    {
+        clrTypeCache = cache;
+        typeAliases = ImportTypeAliases(importDirectives);
     }
 }
--- src/Todl.Compiler/CodeAnalysis/Symbols/VariableSymbol.cs
+++ src/Todl.Compiler/CodeAnalysis/Symbols/VariableSymbol.cs
@@ -1,11 +1,8 @@
-using System;
+﻿namespace Todl.Compiler.CodeAnalysis.Symbols;
 
-namespace Todl.Compiler.CodeAnalysis.Symbols
+public abstract class VariableSymbol : Symbol
 {
-    public abstract class VariableSymbol : Symbol
-    {
-        public abstract bool ReadOnly { get; }
-        public abstract TypeSymbol Type { get; }
-        public virtual bool Constant => false;
-    }
+    public abstract bool ReadOnly { get; }
+    public abstract TypeSymbol Type { get; }
+    public virtual bool Constant => false;
 }
