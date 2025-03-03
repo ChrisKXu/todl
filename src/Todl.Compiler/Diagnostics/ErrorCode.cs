753c63cc8ecf2e3f430abb6144cb826f081e9d70
Chris Xu <chriskxu.tictac@gmail.com>
2025-03-02 16:50:55 -0800

    experimental: implement LoopLabel parsing

--- src/Todl.Compiler/Diagnostics/ErrorCode.cs
+++ src/Todl.Compiler/Diagnostics/ErrorCode.cs
@@ -14,6 +14,7 @@
         IfUnlessKeywordMismatch,
         DuplicateBareElseClauses,
         MisplacedBareElseClauses,
+        InvalidLoopLabel,
 
         // BinderErrors
         UndeclaredVariable,
