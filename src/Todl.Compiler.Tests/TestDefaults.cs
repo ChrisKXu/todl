9a63968301ecc4bc8e41053604f65ba1cafb5444
Chris Xu <chriskxu.tictac@gmail.com>
2025-12-26 15:19:46 -0800

    Fixing DefaultTypeCache in tests (#134)
    * Fixing DefaultTypeCache in tests
    
    * configure vscode

--- src/Todl.Compiler.Tests/TestDefaults.cs
+++ src/Todl.Compiler.Tests/TestDefaults.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using Todl.Compiler.CodeAnalysis;
 using Todl.Compiler.CodeAnalysis.Binding;
 
@@ -14,16 +16,34 @@ static class TestDefaults
 
     static TestDefaults()
     {
-        var assemblyPaths = AppDomain.CurrentDomain.GetAssemblies()
-            .Where(a => !a.IsDynamic)
-            .Select(a => a.Location).Distinct().ToList();
+        // Get all assemblies from the runtime directory to ensure all framework types are available
+        // This is more robust than AppDomain.CurrentDomain.GetAssemblies() which only includes loaded assemblies
+        var runtimeDirectory = RuntimeEnvironment.GetRuntimeDirectory();
+        var runtimeAssemblies = Directory.GetFiles(runtimeDirectory, "*.dll");
+
+        // Also include assemblies from the current AppDomain (for test project assemblies like TestClass)
+        var appDomainAssemblies = AppDomain.CurrentDomain.GetAssemblies()
+            .Where(a => !a.IsDynamic && !string.IsNullOrEmpty(a.Location))
+            .Select(a => a.Location);
+
+        var assemblyPaths = runtimeAssemblies
+            .Concat(appDomainAssemblies)
+            .Distinct()
+            .ToList();
 
         var resolver = new PathAssemblyResolver(assemblyPaths);
         MetadataLoadContext = new MetadataLoadContext(resolver, typeof(object).Assembly.GetName().FullName);
 
         foreach (var assemblyPath in assemblyPaths)
         {
-            MetadataLoadContext.LoadFromAssemblyPath(assemblyPath);
+            try
+            {
+                MetadataLoadContext.LoadFromAssemblyPath(assemblyPath);
+            }
+            catch
+            {
+                // Some assemblies may fail to load (native, etc.) - skip them
+            }
         }
 
         DefaultClrTypeCache = ClrTypeCache.FromAssemblies(assemblies: MetadataLoadContext.GetAssemblies(), MetadataLoadContext.CoreAssembly);
