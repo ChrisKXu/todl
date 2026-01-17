78d488bccdda03395a5033977beb0091fe56d7d4
Chris Xu <chriskxu.tictac@gmail.com>
2026-01-16 17:44:09 -0800

    Adding more tests to Parser (#139)
    * Adding more tests to parser
    
    * more tests

--- src/Todl.Compiler.Tests/CodeAnalysis/ParserTests/BinaryExpressionTests.cs
+++ src/Todl.Compiler.Tests/CodeAnalysis/ParserTests/BinaryExpressionTests.cs
@@ -161,4 +161,102 @@ public sealed class BinaryExpressionTests
         binaryExpression.Operator.Kind.Should().Be(SyntaxKind.PlusToken);
         binaryExpression.Right.As<LiteralExpression>().Text.Should().Be("4");
     }
+
+    [Theory]
+    [InlineData("a & b", "&", SyntaxKind.AmpersandToken)]
+    [InlineData("a | b", "|", SyntaxKind.PipeToken)]
+    [InlineData("a && b", "&&", SyntaxKind.AmpersandAmpersandToken)]
+    [InlineData("a || b", "||", SyntaxKind.PipePipeToken)]
+    public void TestParseBinaryExpressionWithBitwiseAndLogicalOperators(string input, string expectedOperatorText, SyntaxKind expectedKind)
+    {
+        var binaryExpression = TestUtils.ParseExpression<BinaryExpression>(input);
+
+        binaryExpression.Should().NotBeNull();
+        binaryExpression.Left.As<SimpleNameExpression>().Text.Should().Be("a");
+        binaryExpression.Operator.Text.Should().Be(expectedOperatorText);
+        binaryExpression.Operator.Kind.Should().Be(expectedKind);
+        binaryExpression.Right.As<SimpleNameExpression>().Text.Should().Be("b");
+    }
+
+    [Theory]
+    [InlineData("a < b", "<", SyntaxKind.LessThanToken)]
+    [InlineData("a > b", ">", SyntaxKind.GreaterThanToken)]
+    [InlineData("a <= b", "<=", SyntaxKind.LessThanOrEqualsToken)]
+    [InlineData("a >= b", ">=", SyntaxKind.GreaterThanOrEqualsToken)]
+    public void TestParseBinaryExpressionWithComparisonOperators(string input, string expectedOperatorText, SyntaxKind expectedKind)
+    {
+        var binaryExpression = TestUtils.ParseExpression<BinaryExpression>(input);
+
+        binaryExpression.Should().NotBeNull();
+        binaryExpression.Left.As<SimpleNameExpression>().Text.Should().Be("a");
+        binaryExpression.Operator.Text.Should().Be(expectedOperatorText);
+        binaryExpression.Operator.Kind.Should().Be(expectedKind);
+        binaryExpression.Right.As<SimpleNameExpression>().Text.Should().Be("b");
+    }
+
+    [Fact]
+    public void TestParseBinaryExpressionWithDivision()
+    {
+        var binaryExpression = TestUtils.ParseExpression<BinaryExpression>("10 / 2");
+
+        binaryExpression.Should().NotBeNull();
+        binaryExpression.Left.As<LiteralExpression>().Text.Should().Be("10");
+        binaryExpression.Operator.Text.Should().Be("/");
+        binaryExpression.Operator.Kind.Should().Be(SyntaxKind.SlashToken);
+        binaryExpression.Right.As<LiteralExpression>().Text.Should().Be("2");
+    }
+
+    [Fact]
+    public void TestComplexBooleanExpressionPrecedence()
+    {
+        // a && b || c && d should be parsed as ((a && b) || (c && d))
+        // because && has higher precedence than ||
+        var binaryExpression = TestUtils.ParseExpression<BinaryExpression>("a && b || c && d");
+
+        binaryExpression.Should().NotBeNull();
+        binaryExpression.Operator.Kind.Should().Be(SyntaxKind.PipePipeToken);
+
+        var left = binaryExpression.Left.As<BinaryExpression>();
+        left.Operator.Kind.Should().Be(SyntaxKind.AmpersandAmpersandToken);
+        left.Left.As<SimpleNameExpression>().Text.Should().Be("a");
+        left.Right.As<SimpleNameExpression>().Text.Should().Be("b");
+
+        var right = binaryExpression.Right.As<BinaryExpression>();
+        right.Operator.Kind.Should().Be(SyntaxKind.AmpersandAmpersandToken);
+        right.Left.As<SimpleNameExpression>().Text.Should().Be("c");
+        right.Right.As<SimpleNameExpression>().Text.Should().Be("d");
+    }
+
+    [Fact]
+    public void TestBitwiseOperatorPrecedence()
+    {
+        // a | b & c should be parsed as (a | (b & c))
+        // because & has higher precedence than |
+        var binaryExpression = TestUtils.ParseExpression<BinaryExpression>("a | b & c");
+
+        binaryExpression.Should().NotBeNull();
+        binaryExpression.Operator.Kind.Should().Be(SyntaxKind.PipeToken);
+        binaryExpression.Left.As<SimpleNameExpression>().Text.Should().Be("a");
+
+        var right = binaryExpression.Right.As<BinaryExpression>();
+        right.Operator.Kind.Should().Be(SyntaxKind.AmpersandToken);
+        right.Left.As<SimpleNameExpression>().Text.Should().Be("b");
+        right.Right.As<SimpleNameExpression>().Text.Should().Be("c");
+    }
+
+    [Fact]
+    public void TestMixedArithmeticAndComparisonPrecedence()
+    {
+        // a + b < c * d should be parsed as ((a + b) < (c * d))
+        var binaryExpression = TestUtils.ParseExpression<BinaryExpression>("a + b < c * d");
+
+        binaryExpression.Should().NotBeNull();
+        binaryExpression.Operator.Kind.Should().Be(SyntaxKind.LessThanToken);
+
+        var left = binaryExpression.Left.As<BinaryExpression>();
+        left.Operator.Kind.Should().Be(SyntaxKind.PlusToken);
+
+        var right = binaryExpression.Right.As<BinaryExpression>();
+        right.Operator.Kind.Should().Be(SyntaxKind.StarToken);
+    }
 }
